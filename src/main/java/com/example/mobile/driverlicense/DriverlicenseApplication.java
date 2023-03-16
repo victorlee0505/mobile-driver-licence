00cdd84383a41f87fb1b5326ad21c4634d7f7ea1
Victor Lee <victorlee0505@gmail.com>
2023-03-16 11:04:45 -0400

    First commit
    Signed-off-by: Victor Lee <victorlee0505@gmail.com>

---
+++ src/main/java/com/example/mobile/driverlicense/DriverlicenseApplication.java
@@ -0,0 +1,13 @@
+package com.example.mobile.driverlicense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverlicenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverlicenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mobile/driverlicense/DriverlicenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mobile.driverlicense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverlicenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
