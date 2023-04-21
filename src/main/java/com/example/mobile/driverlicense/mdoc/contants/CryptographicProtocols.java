b371ed018c51ce0f8899c9d21f410d61a913f160
Victor Lee <victorlee0505@gmail.com>
2023-04-21 14:25:47 -0400

    contants for mDL
    Signed-off-by: Victor Lee <victorlee0505@gmail.com>

---
+++ src/main/java/com/example/mobile/driverlicense/mdoc/contants/CryptographicProtocols.java
@@ -0,0 +1,25 @@
+package com.example.mobile.driverlicense.mdoc.contants;
+
+public class CryptographicProtocols {
+    
+    /** 1.2.840.10045.2.1 (Elliptic curve) */
+    public static final String ELLIPTIC_CURVE = "EC";
+
+    /** 9.2.1 TLS RFC 8422 SHA256 */
+    public static final String SECP256R1 = "secp256r1";
+
+    /** 9.2.1 TLS RFC 8422 SHA256 */
+    public static final String SECP384R1 = "secp384r1";
+
+    /** 9.2.1 TLS RFC 8422 SHA256 */
+    public static final String SECP521R1 = "secp521r1";
+
+    /** 9.1.2.5 Digest algorithm SHA-256 */
+    public static final String DIGEST_SHA_256 = "SHA-256";
+
+    /** 9.1.2.5 Digest algorithm SHA-384 */
+    public static final String DIGEST_SHA_384 = "SHA-384";
+
+    /** 9.1.2.5 Digest algorithm SHA-512 */
+    public static final String DIGEST_SHA_512 = "SHA-512";
+}
