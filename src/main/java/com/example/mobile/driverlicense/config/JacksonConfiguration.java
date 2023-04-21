121ea8feec3466c2e99050cb76cd9311fd2eec26
Victor Lee <victorlee0505@gmail.com>
2023-04-21 14:27:08 -0400

    JacksonConfiguration
    Signed-off-by: Victor Lee <victorlee0505@gmail.com>

---
+++ src/main/java/com/example/mobile/driverlicense/config/JacksonConfiguration.java
@@ -0,0 +1,17 @@
+package com.example.mobile.driverlicense.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class JacksonConfiguration {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return mapper;
+    }
+}
